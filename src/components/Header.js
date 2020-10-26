448f0675aee318815bd2bbc2c251090552ca0b8f
hmshreyas7 <hmshreyas7@gmail.com>
2020-10-26 11:13:48 -0700

    Add required parts of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const [searchText, setSearchText] = useState('');
+
+    const handleChange = (event) => {
+        const inputValue = event.target.value;
+        setSearchText(inputValue);
+    }
+
     return <header>
         <div>
             <div><Link to='/home'>Home</Link></div>
             <div><Link to='/following'>Following</Link></div>
-            <div><input /></div>
+            <div>
+                <input onChange={handleChange} value={searchText} />
+                <button>Search</button>
+            </div>
         </div>
     </header>
 }
