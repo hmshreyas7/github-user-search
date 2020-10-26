c150e0e8c8f041b97383f3f70dc1749412fc3e33
hmshreyas7 <hmshreyas7@gmail.com>
2020-10-26 11:17:31 -0700

    Initialize other routes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
+    <Header />
     <Switch>
+      <Route path="/home"></Route>
+      <Route path="/following"></Route>
       <Route path="/">
-        <Header />
+        <p>Use the search bar above to fetch data for a GitHub user.</p>
       </Route>
     </Switch>
     </div>
