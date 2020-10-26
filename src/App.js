fad6e6fdea9494acc30a723bedad3647e7165d40
hmshreyas7 <hmshreyas7@gmail.com>
2020-10-26 11:10:38 -0700

    Set up router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+    <Switch>
+      <Route path="/">
+        <Header />
+      </Route>
+    </Switch>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-    return <header>Github User Search</header>
+    return <header>
+        <div>
+            <div><Link to='/home'>Home</Link></div>
+            <div><Link to='/following'>Following</Link></div>
+            <div><input /></div>
+        </div>
+    </header>
 }
 
 export default Header;
